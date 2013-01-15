6956e87962427f6692c394f0a837c0fae05e3e33
Wil Boayue <wil.boayue@gmail.com>
2013-01-15 18:43:45 -0500

    added test for rates=

--- 01-international-trade/test/test_rate_loader.rb
+++ 01-international-trade/test/test_rate_loader.rb
@@ -11,20 +11,41 @@ describe RateHash do
 
   describe "#load" do
     before do
-      @rates_data_file = File.join(File.dirname(__FILE__), 'data/sample_rates.xml')
-      @rates.load @rates_data_file
+      @rates.load rates_data_file
     end
 
     it "should load rates into hash" do
-      refute @rates[:aud, :cad].nil?, 'no rates for aud -> cad'
-      assert_in_delta @rates[:aud, :cad], 1.0079, 0.00001
-
-      refute @rates[:cad, :usd].nil?, 'no rates for cad -> usd'
-      assert_in_delta @rates[:cad, :usd], 1.0090, 0.00001
-      
-      refute @rates[:usd, :cad].nil?, 'no rates for usd -> cad'
-      assert_in_delta @rates[:usd, :cad], 0.9911, 0.00001
+      assert_that_rates_are_loaded
     end
+  end
+
+  describe "#rates=" do
+    before do
+      @rates.rates = {
+        aud: {cad: 1.0079},
+        cad: {usd: 1.0090},
+        usd: {cad: 0.9911}
+      }
+    end
+
+    it "should populate rates" do
+      assert_that_rates_are_loaded
+    end
+  end
+
+  def rates_data_file
+    File.join(File.dirname(__FILE__), 'data/sample_rates.xml')
+  end
+
+  def assert_that_rates_are_loaded
+    refute @rates[:aud, :cad].nil?, 'no rates for aud -> cad'
+    assert_in_delta @rates[:aud, :cad], 1.0079, 0.00001
+
+    refute @rates[:cad, :usd].nil?, 'no rates for cad -> usd'
+    assert_in_delta @rates[:cad, :usd], 1.0090, 0.00001
+    
+    refute @rates[:usd, :cad].nil?, 'no rates for usd -> cad'
+    assert_in_delta @rates[:usd, :cad], 0.9911, 0.00001
   end
 
 end
