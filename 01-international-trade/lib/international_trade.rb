13f6f256a0604a5fc54dbbbd50033caba9cacf48
Wil Boayue <wil.boayue@gmail.com>
2013-01-19 19:39:19 -0600

    implement command line options

--- 01-international-trade/lib/international_trade.rb
+++ 01-international-trade/lib/international_trade.rb
@@ -1,6 +1,22 @@
 require 'international_trade/currency_converter'
 require 'international_trade/rate_hash'
 require 'international_trade/transaction_iterator'
+require 'international_trade/sales_totaler'
 
 class InternationalTrade
+  
+  attr_accessor :rates, :transactions
+
+  def initialize(arguments)
+    @rates = arguments[:rates]
+    @transactions = arguments[:transactions]
+  end
+
+  def compute_grand_total(sku, currency)
+    CurrencyConverter.load_conversions(rates)
+    trans = TransactionIterator.new(transactions)
+    sales_totaler = SalesTotaler.new transactions: trans
+    sales_totaler.compute_grand_total sku.upcase, currency
+  end
+
 end
