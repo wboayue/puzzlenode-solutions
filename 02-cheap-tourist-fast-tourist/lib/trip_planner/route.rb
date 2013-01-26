f19b7886daca6a37949392847313211d2d2d9099
Wil Boayue <wil.boayue@gmail.com>
2013-01-25 23:25:46 -0500

    removed duplication

--- 02-cheap-tourist-fast-tourist/lib/trip_planner/route.rb
+++ 02-cheap-tourist-fast-tourist/lib/trip_planner/route.rb
@@ -23,28 +23,28 @@ class Route
   end
 
   def cheaper_than?(other)
-    return true if other.nil?
-
-    if self.cost == other.cost
-      self.duration < other.duration
-    else
-      self.cost < other.cost
-    end
+    less_than?(other, :cost, :duration)
   end
 
   def faster_than?(other)
-    return true if other.nil?
-
-    if self.duration == other.duration
-      self.cost < other.cost
-    else
-      self.duration < other.duration
-    end
+    less_than?(other, :duration, :cost)
   end
 
   def to_s
     return "" if edges.empty?
     "#{format_time(first_flight.depart_at)} #{format_time(last_flight.arrive_at)} #{'%.2f' % cost}"
+  end
+
+  private 
+
+  def less_than?(other, primary_attribute, secondary_attribute)
+    return true if other.nil?
+
+    if self.send(primary_attribute) == other.send(primary_attribute)
+      self.send(secondary_attribute) < other.send(secondary_attribute)
+    else
+      self.send(primary_attribute) < other.send(primary_attribute)
+    end
   end
 
   def format_time(time)
