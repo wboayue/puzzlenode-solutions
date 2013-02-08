8ee4498b0f75c5072eefb0e61ad4a60036af762d
Wil Boayue <wil.boayue@gmail.com>
2013-02-08 16:01:50 -0500

    cleanup output

--- 07-blue-hawaii/lib/blue_hawaii.rb
+++ 07-blue-hawaii/lib/blue_hawaii.rb
@@ -12,14 +12,12 @@ class BlueHawaii
 
   def solve
     raw_date_range = File.read(@input_path)
-    pp raw_date_range
 
     date_range = raw_date_range.split("-").collect &:strip
 
     rental_data = JSON.parse File.read(@rental_path)
     
     rental_data.each do |scenario|
-      pp scenario
       rental = RentalUnit.new json: scenario
       rental_cost = rental.rental_cost *date_range
       puts "#{rental.name}: $#{ '%.2f' % rental_cost}"
