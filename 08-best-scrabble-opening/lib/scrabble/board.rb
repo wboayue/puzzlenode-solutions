082ed2fd5a5e0c928439d56c88df79596442e3f8
Wil Boayue <wil.boayue@gmail.com>
2013-02-09 21:02:36 -0500

    score board

--- 08-best-scrabble-opening/lib/scrabble/board.rb
+++ 08-best-scrabble-opening/lib/scrabble/board.rb
@@ -6,6 +6,7 @@ class Board
 
   def initialize(options)
     populate_board(options[:board] || [])
+    @tile_bag = options[:tile_bag]
   end
 
   def columns
@@ -20,7 +21,29 @@ class Board
     @cells[row][column]
   end
 
+  def score(row, column, word, direction = :across)
+    direction == :across ? score_across(row, column, word) : score_down(row, column, word)
+  end
+
   private
+
+  def score_across(row, column, word)
+    result = 0
+    return result if column + word.size > columns
+    (column...(column + word.size)).each do |i|
+      result += @tile_bag.points(word[i-column].to_sym) * @cells[row][i]
+    end
+    result
+  end
+
+  def score_down(row, column, word)
+    result = 0
+    return result if row + word.size > rows
+    (row...(row + word.size)).each do |i|
+      result += @tile_bag.points(word[i-row].to_sym) * @cells[i][column]
+    end
+    result
+  end
 
   def populate_board(board)
     @cells = []
--- 08-best-scrabble-opening/spec/board_spec.rb
+++ 08-best-scrabble-opening/spec/board_spec.rb
@@ -1,34 +1,58 @@
 require 'scrabble/board'
 
 describe Board do
-  subject do
-    board =[
-      "1 1 1 1 1 1 1 1 1 1 1 1",
-      "1 1 1 2 1 2 1 1 1 1 1 1",
-      "1 2 1 1 1 3 1 1 1 1 2 1",
-      "2 1 1 1 1 1 1 1 1 2 2 1",
-      "1 1 1 2 1 1 1 1 1 1 1 1",
-      "1 1 1 1 1 1 2 1 1 1 2 1",
-      "1 1 1 1 1 1 1 1 2 1 1 1",
-      "1 1 1 1 1 1 1 1 1 1 1 2",
-      "1 1 1 1 1 1 1 1 1 1 1 1"
-    ]
-
-    Board.new board: board
-  end
-
   describe "#initialize" do
+    before do
+      board =[
+        "1 1 1 1 1 1 1 1 1 1 1 1",
+        "1 1 1 2 1 2 1 1 1 1 1 1",
+        "1 2 1 1 1 3 1 1 1 1 2 1",
+        "2 1 1 1 1 1 1 1 1 2 2 1",
+        "1 1 1 2 1 1 1 1 1 1 1 1",
+        "1 1 1 1 1 1 2 1 1 1 2 1",
+        "1 1 1 1 1 1 1 1 2 1 1 1",
+        "1 1 1 1 1 1 1 1 1 1 1 2",
+        "1 1 1 1 1 1 1 1 1 1 1 1"
+      ]
+
+      @board = Board.new board: board
+    end
+
     it "should set rows and columns" do
-      subject.rows.should == 9
-      subject.columns.should == 12
+      @board.rows.should == 9
+      @board.columns.should == 12
     end
 
     it "should set multipliers" do
-      subject[0, 0].should == 1
-      subject[7, 11].should == 2
-      subject[2, 5].should == 3
+      @board[0, 0].should == 1
+      @board[7, 11].should == 2
+      @board[2, 5].should == 3
     end
   end
 
+  describe "#score" do
+    before do
+      board =[
+        "1 1 1 1 1",
+        "1 1 1 2 1",
+        "1 2 1 1 1",
+        "2 1 1 1 1",
+      ]
+
+      @tile_bag = double('tile_bag')
+      @board = Board.new board: board, tile_bag: @tile_bag
+    end
+
+    it "should score words" do
+      @tile_bag.should_receive(:points).any_number_of_times.with(:s).and_return(3)
+      @tile_bag.should_receive(:points).any_number_of_times.with(:a).and_return(4)
+      @tile_bag.should_receive(:points).any_number_of_times.with(:w).and_return(5)
+
+      @board.score(0, 0, "saw", :across).should == 12
+      @board.score(0, 3, "saw", :down).should == 16
+      @board.score(2, 0, "saw", :down).should == 0
+    end
+
+  end
 
 end
